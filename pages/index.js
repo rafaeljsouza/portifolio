65258a73c2963eb639f4d66366bfd47b63c5ebee
Rafael Souza <rafaelsouza@cogmonkeys.com>
2025-01-26 23:51:07 -0300

    changed middleware to redirect

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    fetch('/api/redirect')
+      .then(response => {
+        if (response.redirected) {
+          window.location.href = response.url
+        }
+      })
+  }, [])
+
   return null
 }

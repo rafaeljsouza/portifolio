5030af0f748bf062ccfaf5b5be3e63923c445998
Rafael Souza <rafaelsouza@cogmonkeys.com>
2025-01-26 23:35:46 -0300

    removed middleware, changed index and next.config

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import { useRouter } from 'next/router'
-
 export default function Home() {
-  const router = useRouter()
-  
-  if (typeof window !== 'undefined') {
-    router.replace('/pt')
-  }
-  
   return null
+}
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/pt',
+      permanent: false,
+    },
+  }
 }

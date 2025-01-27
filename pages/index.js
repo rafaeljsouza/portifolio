0df4c79f3a216689ce01c91791db475d1660f117
Rafael Souza <rafaelsouza@cogmonkeys.com>
2025-01-27 16:00:12 -0300

    mudancas de roteamento de novo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import { useRouter } from 'next/router'
-
 export default function Home() {
-  const router = useRouter()
-  if (typeof window !== 'undefined') {
-    router.push('/pt')
-  }
   return null
 }
